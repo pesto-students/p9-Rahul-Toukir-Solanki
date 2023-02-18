49958279b60051fc0c228b495aff0ab5ca169422
Toukir-Solanki <toukirsolanki@gmail.com>
2023-02-18 11:43:22 +0530

    Creating 3 simple functions where call, bind and apply are used

---
+++ Week-3/exercise3.2/script.js
@@ -0,0 +1,24 @@
+// This is the variable (form)
+let form = {
+  name: "Toukir",
+  lastName: "Solanki",
+};
+
+//This is the function which we use
+let fullName = function (state, fun) {
+  return (
+    this.name + " " + this.lastName + " " + state + " using function " + fun
+  );
+};
+
+// Using Call function
+var call = fullName.call(form, "Raipur", "Call");
+console.log(call);
+
+//using Apply function
+let apply = fullName.apply(form, ["raipur", "Apply"]);
+console.log(apply);
+
+//using Bind function
+let bind = fullName.bind(form, "Raipur", "Bind");
+console.log(bind());
